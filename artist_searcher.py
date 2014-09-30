021ddd87885727ed67e5c47a844f8424ceff46e0
Jamie Lawler <jamie.lawler@gmail.com>
2014-09-30 22:55:57 +0100

    better login system

--- artist_searcher.py
+++ artist_searcher.py
@@ -18,7 +18,9 @@ def main():
         
     session.connection.state
     ###############
-    session.login("user", "password")
+    user_login = input("Please input your username")
+    user_password = input("Please input your password")
+    session.login(user_login,user_password)
     ###############
     session.connection.state
     logged_in_event.wait()
@@ -26,16 +28,21 @@ def main():
     ##################################
     
     print(str(session.user))
-    user_search = input("Please enter a band name:\n")
-    search = session.search(user_search) #do the search
-    search.load() # load the search results
-    album = session.get_album(str(search.albums[0].link)) # store the first album of the search in the album variable
-    ## user browser.load() as a workaround: album.load() seems to be broken 2014-09-30
-    browser = album.browse()
-    browser.load()
-    
-    print("Their most popular album is " + str(album.name))
-    cover = album.cover(spotify.ImageSize.LARGE)
-    cover.load()
-    open('/tmp/cover.html', 'w+').write('<img src="%s"> <h1>%s - %s</h1>' % (cover.data_uri, album.artist.name, album.name))
+    search_active = True
+    while search_active:
+        user_search = input("Please enter a band name (type\"!quit\" to exit):\n")
+        if "!quit" in user_search:
+            search_active = False
+            break
+        search = session.search(user_search) #do the search
+        search.load() # load the search results
+        album = session.get_album(str(search.albums[0].link)) # store the first album of the search in the album variable
+        ## user browser.load() as a workaround: album.load() seems to be broken 2014-09-30
+        browser = album.browse()
+        browser.load()
+        
+        print("Their most recent album is " + str(album.name))
+        cover = album.cover(spotify.ImageSize.LARGE)
+        cover.load()
+        open('/tmp/cover.html', 'w+').write('<img src="%s"> <h1>%s - %s</h1>' % (cover.data_uri, album.artist.name, album.name))
 main()
