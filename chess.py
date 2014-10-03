066eaf3062dcf889f96fe3d5d0d7a583cbab592e
Jamie Lawler <jamie.lawler@gmail.com>
2014-10-03 18:43:23 +0100

    generating pieces in saner way, capturing pieces mostly implemented

--- chess.py
+++ chess.py
@@ -5,44 +5,30 @@ def main():
             check for check
             add win conditions
             alternate between black and white
-            make pieces in a better way
             handle user input more elegantly
             handle bad user input
             add special cases (castling, promotion, en passant)
     """
     import sys
+    
     board_size = 8
     empty_square = "-"
-    board = [[empty_square for j in range(board_size)] for i in range(board_size)]
+    piece_on_square_colour = ""
     move_piece_located_at = [0,0]
     move_piece_to = [0,0]
-
-    w_pawn = []
-    b_pawn = []
-    w_rook = []
-    w_bishop = []
-    w_knight = []
-    w_king = []
-    w_queen = []
-    b_rook = []
-    b_bishop = []
-    b_knight = []
-    b_king = []
-    b_queen = []
-
-    all_pieces = [w_pawn, b_pawn, w_rook, w_bishop, w_knight, w_king,
-        w_queen, b_rook, b_bishop, b_knight, b_king, b_queen]
-
+    game_over = False
+    turn_counter = 1
+    pieces_in_play = []
+    captured_pieces = []
+    board = [[empty_square for j in range(board_size)] for i in range(board_size)]
+    
     def update_board():
-        for type_of_piece in all_pieces:
-            for piece in type_of_piece:
-                for row in range(board_size):
-                    for column in range(board_size):
-                        if piece.row == row and piece.column == column:
-                            board[row][column] = piece.icon
+        for piece in pieces_in_play:
+            for row in range(board_size):
+                for column in range(board_size):
+                    if piece.row == row and piece.column == column:
+                        board[row][column] = piece.icon
         print_board()
-
-    ## probably could use a function "is_piece_on_square" or similar. reuse code from update board in the collision function?
 
     def print_board():
         for row in range(board_size):
@@ -55,6 +41,24 @@ def main():
             print(column, end = " ")
         print()
 
+    def piece_on_square(row,column):
+        piece_on_square_var = False
+        for piece in pieces_in_play:
+            if piece.row == row and piece.column == column:
+                piece_on_square_var = True
+                piece_on_square_colour = piece.colour
+        if piece_on_square_var:
+            return True
+        else:
+            return False
+            
+    def capture_piece(row,column):
+        for piece in pieces_in_play:
+            if piece.row == row and piece.column == column:
+                captured_pieces.append(piece)
+                pieces_in_play.remove(piece)
+                print("Captured ", captured_pieces[-1].icon)
+                    
     class Piece:
         def __init__(self,name,colour,icon,row,column,proposed_row,proposed_column,):
             self.name = name
@@ -67,23 +71,23 @@ def main():
 
     def create_pawns():
         for i in range(board_size):
-            w_pawn.append(Piece("pawn","white","♙",6,i,0,0,))
-            b_pawn.append(Piece("pawn","black","♟",1,i,0,0,))
+            pieces_in_play.append(Piece("pawn","white","♙",6,i,0,0,))
+            pieces_in_play.append(Piece("pawn","black","♟",1,i,0,0,))
 
     def create_other_pieces():
         for i in range(2):
-            w_rook.append(Piece("rook","white","♖",7,0 + (i * (board_size - 1)),0,0,))
-            b_rook.append(Piece("rook","black","♜",0,0 + (i * (board_size - 1)),0,0,))
-            w_knight.append(Piece("knight","white","♘",7,1 + (i * (board_size - 3)),0,0,))
-            b_knight.append(Piece("knight","black","♞",0,1 + (i * (board_size - 3)),0,0,))
-            w_bishop.append(Piece("bishop","white","♗",7,2 + (i * (board_size - 5)),0,0,))
-            b_bishop.append(Piece("bishop","black","♝",0,2 + (i * (board_size - 5)),0,0,))
+            pieces_in_play.append(Piece("rook","white","♖",7,0 + (i * (board_size - 1)),0,0,))
+            pieces_in_play.append(Piece("rook","black","♜",0,0 + (i * (board_size - 1)),0,0,))
+            pieces_in_play.append(Piece("knight","white","♘",7,1 + (i * (board_size - 3)),0,0,))
+            pieces_in_play.append(Piece("knight","black","♞",0,1 + (i * (board_size - 3)),0,0,))
+            pieces_in_play.append(Piece("bishop","white","♗",7,2 + (i * (board_size - 5)),0,0,))
+            pieces_in_play.append(Piece("bishop","black","♝",0,2 + (i * (board_size - 5)),0,0,))
 
     def create_royalty():
-        w_king.append(Piece("king","white","♔",7,4,0,0,))
-        b_king.append(Piece("king","black","♚",0,4,0,0,))
-        w_queen.append(Piece("queen","white","♕",7,3,0,0,))
-        b_queen.append(Piece("queen","black","♛",0,3,0,0,))
+        pieces_in_play.append(Piece("king","white","♔",7,4,0,0,))
+        pieces_in_play.append(Piece("king","black","♚",0,4,0,0,))
+        pieces_in_play.append(Piece("queen","white","♕",7,3,0,0,))
+        pieces_in_play.append(Piece("queen","black","♛",0,3,0,0,))
 
     def create_pieces():
         create_pawns()
@@ -94,29 +98,28 @@ def main():
         user_choice = input("Choose a piece (row,column)")
         if "quit" in user_choice.lower():
             sys.exit("Exiting...")
-        #move_piece_located_at = [0,0]
         move_piece_located_at[0] = int(user_choice[0])
         move_piece_located_at[1] = int(user_choice[2])
         user_move = input("Choose where to move the piece (row,column)")
         if "quit" in user_move.lower():
             sys.exit("Exiting...2")
-        #move_piece_to = [0,0]
         move_piece_to[0] = int(user_move[0])
         move_piece_to[1] = int(user_move[2])
-        for type_of_piece in all_pieces:
-            for piece in type_of_piece:
-                if piece.row == move_piece_located_at[0] and piece.column == move_piece_located_at[1]:
-                    piece.proposed_row = move_piece_to[0]
-                    piece.proposed_column = move_piece_to[1]
-                    if move_legality(piece) and collision_detection(piece):
-                        # blank old location
-                        board[piece.row][piece.column] = empty_square
-                        piece.row = piece.proposed_row
-                        piece.column = piece.proposed_column
-                        # Do we need to clear proposed_row and _column here?
-                    else:
-                        print("Sorry, that move is invalid")
-                        move_piece()
+        for piece in pieces_in_play:
+            if piece.row == move_piece_located_at[0] and piece.column == move_piece_located_at[1]:
+                piece.proposed_row = move_piece_to[0]
+                piece.proposed_column = move_piece_to[1]
+                if move_legality(piece) and collision_detection(piece):
+                    # blank old location
+                    board[piece.row][piece.column] = empty_square
+                    if piece_on_square(piece.proposed_row,piece.proposed_column):
+                        capture_piece(piece.proposed_row,piece.proposed_column)
+                    piece.row = piece.proposed_row
+                    piece.column = piece.proposed_column
+                    # Do we need to clear proposed_row and _column here?
+                else:
+                    print("Sorry, that move is invalid")
+                    move_piece()
 
     def move_legality(piece):
         if piece.name == "bishop":
@@ -133,10 +136,10 @@ def main():
             if pawn_move_legality(piece):
                 return True
         elif piece.name == "knight":
-            if knight_move_legaility(piece):
+            if knight_move_legality(piece):
                 return True
         elif piece.name == "king":
-            if king_move_legality():
+            if king_move_legality(piece):
                 return True
         else:
             return False
@@ -155,20 +158,29 @@ def main():
     def pawn_move_legality(piece):
         if piece.colour == "black":
             if piece.row == 1 and piece.proposed_row == 3 and piece.column == piece.proposed_column:
-                return True
+                # collision code
+                if (not piece_on_square(2,piece.column)) or (not piece_on_square(3,piece.column)):
+                    return True
             elif piece.row == piece.proposed_row - 1 and piece.column == piece.proposed_column:
-                return True
-            elif piece.row == piece.proposed_row - 1 and abs(piece.column - piece.proposed_column) == 1 and capturing_piece():
-                return True
+                if (not piece_on_square(piece.proposed_row,piece.proposed_column)):
+                    return True
+            elif piece.row == piece.proposed_row - 1 and abs(piece.column - piece.proposed_column) == 1:
+                # we're capturing a piece here
+                if piece_on_square(piece.proposed_row,piece.proposed_column) and piece_on_square_colour != "black":
+                    return True
             else:
                 return False
-        if piece.colour == "white":
+                
+        elif piece.colour == "white":
             if piece.row == 6 and piece.proposed_row == 4 and piece.column == piece.proposed_column:
-                return True
+                if (not piece_on_square(5,piece.column)) or (not piece_on_square(4,piece.column)):
+                    return True
             elif piece.row == piece.proposed_row + 1 and piece.column == piece.proposed_column:
-                return True
-            elif piece.row == piece.proposed_row + 1 and abs(piece.column - piece.proposed_column) == 1 and capturing_piece():
-                return True
+                if (not piece_on_square(piece.proposed_row,piece.proposed_column)):
+                    return True
+            elif piece.row == piece.proposed_row + 1 and abs(piece.column - piece.proposed_column) == 1:
+                if piece_on_square(piece.proposed_row,piece.proposed_column) and piece_on_square_colour != "white":
+                    return True
             else:
                 return False
 
@@ -187,15 +199,13 @@ def main():
     def collision_detection(piece):
         return True
 
-    def capturing_piece():
-        return True
 
+    
     print("Remember to set your terminal to black text on a white background!\n")
+    
     create_pieces()
     update_board()
-    game_over = False
-    turn_counter = 1
-    
+
     while not game_over:
         if turn_counter % 2 == 1:
             print("White's turn")
