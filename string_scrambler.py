f63079fa8aee9723393956deda5f0bd5df2d49b9
Jamie Lawler <jamie.lawler@gmail.com>
2014-12-03 16:26:16 +0000

    Initial implementation of a "Cambridge University" string-scrambler and unittests

---
+++ string_scrambler.py
@@ -0,0 +1,56 @@
+import random
+import unittest
+
+
+def randomise_letters(letters):
+    letters = list(letters)
+    random.shuffle(letters)
+    shuffled = "".join(letters)
+    return shuffled
+
+
+def scramble_word(word):
+    if not word[-1].isalpha():
+        return scramble_word(word[0:-1]) + word[-1]
+    if len(word) < 4:
+        return word
+    else:
+        return word[0] + randomise_letters(word[1:-1]) + word[-1]
+
+
+def scramble_phrase(phrase):
+    shuffled_phrase = []
+    phrase = phrase.split(" ")
+    for word in phrase:
+        shuffled_phrase.append(scramble_word(word))
+    joined_shuffled_phrase = " ".join(shuffled_phrase)
+    return joined_shuffled_phrase
+
+
+def main():
+    input_phrase = "The chancellor said that from midnight the current system, where the" \
+                   " amount owed jumps at certain price levels, would be replaced by a " \
+                   "graduated rate, working in a similar way to income tax."
+
+    print(scramble_phrase(input_phrase))
+
+
+class MyScrambleTests(unittest.TestCase):
+    def test_randomise_letters(self):
+        self.assertEqual(randomise_letters("a"), "a")
+
+    def test_scramble_word(self):
+        self.assertEqual(scramble_word("dog"), "dog")
+        self.assertEqual(scramble_word("on"), "on")
+        self.assertEqual(scramble_word("on..!"), "on..!")
+
+    def test_scramble_phrase(self):
+        phrase = "The lazy dog jumped over the blue moon"
+        self.assertEqual(scramble_phrase("on dog"), "on dog")
+        self.assertEqual(scramble_phrase("On dog.!."), "On dog.!.")
+        self.assertCountEqual(scramble_phrase(phrase), phrase)
+
+
+if __name__ == '__main__':
+    main()
+    unittest.main()
