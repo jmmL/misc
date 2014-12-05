5337c377b254b1ef897da60a978a15c4e552efd2
Jamie Lawler <jamie.lawler@gmail.com>
2014-12-05 16:08:40 +0000

    Separate into functions

--- vigenere.py
+++ vigenere.py
@@ -1,27 +1,38 @@
 
-grid = []
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for i in range(0, len(alphabet)):
-    grid.append([])
-for i in range(0, len(alphabet)):
-    grid[i] = alphabet[i:] + alphabet[:i]
-
+def generate_grid():
+    grid = []
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for i in range(0, len(alphabet)):
+        grid.append([])
+    for i in range(0, len(alphabet)):
+        grid[i] = alphabet[i:] + alphabet[:i]
+    return grid
     
-key = "lemon"
-plaintext = "itwasacolddarknight"
-ciphertext = ""
+def encipher_text(plaintext, grid, key):
+    ciphertext = ""
+    for i in range(0, len(plaintext)):
+        ciphertext += grid[ord(key[i % len(key)]) - 97][ord(plaintext[i]) - 97]
+    return ciphertext
 
-key = key.lower()
-plaintext = plaintext.lower()
+def decipher_text(ciphertext, grid, key):
+    attempted_plaintext = ""
+    for i in range(0, len(ciphertext)):
+        attempted_plaintext += chr(grid[ord(key[i % len(key)]) - 97].index(ciphertext[i]) + 97)
+    return attempted_plaintext    
 
-for i in range(0, len(plaintext)):
-    ciphertext += grid[ord(key[i % len(key)]) - 97][ord(plaintext[i]) - 97]
+def main():
+    key = "lemon"
+    plaintext = "itwasacolddarknight"
+
+    key = key.lower()
+    plaintext = plaintext.lower()
+    grid = generate_grid()
+
+    ciphertext = encipher_text(plaintext, grid, key)
+    print(ciphertext)
     
-print(ciphertext)
-
-attempted_plaintext = ""
-
-for i in range(0, len(ciphertext)):
-    attempted_plaintext += chr(grid[ord(key[i % len(key)]) - 97].index(ciphertext[i]) + 97)
+    attempted_plaintext = decipher_text(ciphertext, grid, key)
+    print(attempted_plaintext)
     
-print(attempted_plaintext)
+if __name__ == "__main__":
+    main()
