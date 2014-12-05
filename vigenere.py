7985853f704ea5bf9b2354a829c3e76f6f521d39
Jamie Lawler <jamie.lawler@gmail.com>
2014-12-05 16:13:32 +0000

    Better whitespacing

--- vigenere.py
+++ vigenere.py
@@ -1,4 +1,3 @@
-
 def generate_grid():
     grid = []
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -7,18 +6,21 @@ def generate_grid():
     for i in range(0, len(alphabet)):
         grid[i] = alphabet[i:] + alphabet[:i]
     return grid
-    
+
+   
 def encipher_text(plaintext, grid, key):
     ciphertext = ""
     for i in range(0, len(plaintext)):
         ciphertext += grid[ord(key[i % len(key)]) - 97][ord(plaintext[i]) - 97]
     return ciphertext
 
+
 def decipher_text(ciphertext, grid, key):
     attempted_plaintext = ""
     for i in range(0, len(ciphertext)):
         attempted_plaintext += chr(grid[ord(key[i % len(key)]) - 97].index(ciphertext[i]) + 97)
     return attempted_plaintext    
+
 
 def main():
     key = "lemon"
@@ -30,9 +32,9 @@ def main():
 
     ciphertext = encipher_text(plaintext, grid, key)
     print(ciphertext)
-    
+
     attempted_plaintext = decipher_text(ciphertext, grid, key)
     print(attempted_plaintext)
-    
+
 if __name__ == "__main__":
     main()
